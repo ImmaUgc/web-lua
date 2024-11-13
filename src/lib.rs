dfa000211b62ea5459db58326a4ffec127820a62
Leonardo Natale <1gamercomum@gmail.com>
2024-11-13 17:48:27 -0300

    Added element to table

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::sync::Arc;
-
+use web_sys::{ console, js_sys::Array, window, Document, Element };
 use hematita::{ast::{lexer::Lexer, parser}, compiler, lua_lib::{self}, lua_tuple, lua_value, vm::{value::{Table, Value}, VirtualMachine}};
 use wasm_bindgen::prelude::*;
 
@@ -7,17 +7,24 @@ use wasm_bindgen::prelude::*;
 extern "C" {
     #[wasm_bindgen(js_namespace = document)]
     fn write(text: &str);
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(message: &str);
     fn alert(message: &str);
+}
 
+fn element_to_table<'a>(element: Element) -> Table<'a> {
+    let table = Table::default();
+    let mut editable = table.data.lock().unwrap();
+    editable.insert(lua_value!("content"), Value::String(element.text_content().unwrap().into()));
+
+    drop(editable);
+    table
 }
 
 #[wasm_bindgen]
 pub fn execute_lua(source: &str) {
     /* print - console.log */
     let print = |arguments: Arc<Table>, _: &_| {
-        let args: Vec<String> = arguments.data
+        let arr = Array::new();
+        let args: Vec<_> = arguments.data
         .lock()
         .unwrap()
         .iter()
@@ -29,7 +36,12 @@ pub fn execute_lua(source: &str) {
         })
         .collect();
 
-        log(&args[0]);
+        for arg in args {
+            arr.push(&JsValue::from(arg));
+        }
+
+        console::log(&arr);
+
         Ok(lua_tuple![].arc())
     };
 
@@ -68,10 +80,30 @@ pub fn execute_lua(source: &str) {
         Ok(lua_tuple![].arc())
     };
 
+    let query_selector = |arguments: Arc<Table>, _: &_| {
+        let args: Vec<String> = arguments.data
+        .lock()
+        .unwrap()
+        .iter()
+        .filter_map(|arg| {
+            if arg.1.string().is_none() {
+                return None;
+            }
+            Some(format!("{}", arg.1.string().unwrap()))
+        })
+        .collect();
+        let window = window().expect("Could not fetch window context");
+        let document = window.document().unwrap();
+        let element = document.query_selector(&args[0]).unwrap().unwrap();
+        let table = element_to_table(element);
+        Ok(lua_tuple![table.arc()].arc())
+    };
+
     let document_table = Table::default();
     {
         let mut document_data = document_table.data.lock().unwrap();
         document_data.insert(lua_value!("write"), Value::NativeFunction(&lua_doc_write));
+        document_data.insert(lua_value!("select"), Value::NativeFunction(&query_selector));
     }
 
     let global = {
